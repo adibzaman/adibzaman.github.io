187d50b128bf74bc96fbb6f374942ff500679d54
Adib <adibzaman@gmail.com>
2018-08-13 15:27:36 -0400

    debuggin location issue

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,15 +1,5 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
-  [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
     "San Francisco, California",
     37.7792808,
     -122.4192362
